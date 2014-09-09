054bd0a8231d61c95ea5c52736bce66ca192951e
duggank <CreativeWeep@gmail.com>
2014-09-09 08:40:03 -0400

    Initial Commit V1
    We finished Room, Carpet, Yards, HoursAndMinutes, and Eggs! :D

---
+++ Chapter2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 15, width = 25, area = length * width;
+            Console.WriteLine("The floor space is {0} square feet", area);
+        }
+    }
+}
